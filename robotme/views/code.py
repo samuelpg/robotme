e67185a256347147bd7240a403b566460b21d867
Samuel Parra <samuelparra1@gmail.com>
2018-02-26 19:30:01 -0400

    problem with slashes

--- robotme/views/code.py
+++ robotme/views/code.py
@@ -3,22 +3,22 @@ from robotme import app
 
 #RESTFUL ENDPOINTS FOR CODE EDITOR
 
-@app.route('code/<project_slug>', methods = ['GET', 'POST'])
+@app.route('/code/<project_slug>', methods = ['GET', 'POST'])
 def code(project_slug):
     pass
 
-@app.route('code/<project_slug>/get', methods = ['GET'])
+@app.route('/code/<project_slug>/get', methods = ['GET'])
 def get_code(project_slug):
     pass
 
-@app.route('code/<project_slug>/set', methods = ['POST','PUT'])
+@app.route('/code/<project_slug>/set', methods = ['POST','PUT'])
 def set_code(project_slug):
     pass
 
-@app.route('code/<project_slug>/run', methods = ['GET'])
+@app.route('/code/<project_slug>/run', methods = ['GET'])
 def run_code(project_slug):
     pass
 
-@app.route('code/<project_slug>/stop', methods = ['GET'])
+@app.route('/code/<project_slug>/stop', methods = ['GET'])
 def stop_code(project_slug):
     pass
--- robotme/views/variables.py
+++ robotme/views/variables.py
@@ -3,14 +3,14 @@ from robotme import app
 
 #RESTFUL ENDPOINTS FOR VARIABLES
 
-@app.route('variables/<project_slug>', methods = ['GET', 'POST'])
+@app.route('/variables/<project_slug>', methods = ['GET', 'POST'])
 def variables(project_slug):
     pass
 
-@app.route('variables/<project_slug>/get', methods = ['GET'])
+@app.route('/variables/<project_slug>/get', methods = ['GET'])
 def get_variables(project_slug):
     pass
 
-@app.route('variables/<project_slug>/set', methods = ['POST','PUT'])
+@app.route('/variables/<project_slug>/set', methods = ['POST','PUT'])
 def set_variables(project_slug):
     pass
