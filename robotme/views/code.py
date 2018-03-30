65265e15c26ca1ac06e3c95adce0b8c1a85ce4bd
Samuel Parra <samuelparra1@gmail.com>
2018-03-29 21:30:20 -0400

    code debug #20

--- robotme/views/code.py
+++ robotme/views/code.py
@@ -12,7 +12,7 @@ thread_lock = Lock()
 
 def run_code_thread(project_slug):
     APP_ROOT = os.path.dirname(os.path.abspath(__file__))   # refers to application_top
-    APP_STATIC = os.path.join(APP_ROOT,'projects/'+project_slug+'/code.py')
+    APP_STATIC = os.path.join(APP_ROOT,'/home/pi/robotme/robotme/projects/'+project_slug+'/code.py')
     cmds = ['python',APP_STATIC]
     #cmds = ['python','test.py']
     print("running code")
