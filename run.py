e53d81cf0d2c7e73bc90cf9e06bc1ff97193ddf4
Samuel Parra <samuelparra1@gmail.com>
2018-03-31 18:52:28 -0400

    fixing stuff

--- run.py
+++ run.py
@@ -5,7 +5,6 @@ from robotme import app, socketio, database, setter
 from flask_socketio import SocketIO
 
 database.config_db()
-setter.set_pins()
 
 if __name__ == "__main__":
     socketio.run(host='0.0.0.0', port=8081, app = app)
