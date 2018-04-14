cc4c02cecf2117b9f134f4cbce201753205e185a
Samuel Parra <samuelparra1@gmail.com>
2018-04-14 14:40:45 -0400

    kill and more

--- robotme/static/scripts/tutorial.js
+++ robotme/static/scripts/tutorial.js
@@ -1,15 +1,20 @@
-const tutorial = $("#tutorial-modal")
+const tutorial = $("#tutorial-modal");
+const currentPageText = $("#carousell-current");
 let pages = $(".carousell-page");
 let current
 
 const openTutorial = () =>{
     current = 0;
     pages[current].style = "display: block"
+    currentPageText.html(`${current+1}/${pages.length}`)
+    console.log("A")
     checkButtons();
     tutorial.show();
 }
 
 const closeTutorial = () =>{
+    pages[current].style = "display: none";
+    checkButtons();
     tutorial.hide();
 }
 
@@ -17,6 +22,7 @@ const next = () =>{
     pages[current].style = "display: none"
     current += 1;
     pages[current].style = "display: block"
+    currentPageText.html(`${current+1}/${pages.length}`)
     checkButtons();
 }
 
@@ -24,18 +30,21 @@ const back = () =>{
     pages[current].style = "display: none"
     current -= 1;
     pages[current].style = "display: block"
+    currentPageText.html(`${current+1}/${pages.length}`)
     checkButtons();
 }
 
 const checkButtons = () =>{
-    console.log("a")
     if(current == 0){
-        console.log("a")
         $("#back").attr('disabled', true);
         $("#back").addClass('disabled');
+        $("#next").attr('disabled', false);
+        $("#next").removeClass('disabled');
     }else if(current == pages.length - 1){
         $("#next").attr('disabled', true);
         $("#next").addClass('disabled');
+        $("#back").attr('disabled', false);
+        $("#back").removeClass('disabled')
     }else{
         $("#back").attr('disabled', false);
         $("#back").removeClass('disabled')
