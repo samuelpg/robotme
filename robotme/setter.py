77bfe259b4da2b556c71ada3ae6f2be166fd690e
Samuel Parra <samuelparra1@gmail.com>
2018-04-14 14:45:04 -0400

    fun

--- robotme/setter.py
+++ robotme/setter.py
@@ -4,9 +4,6 @@ from robotme import app
 def set_pins():
     pi = pigpio.pi()
     output_pins = app.config['GPIO_OUTPUT']
-    input_pins = app.config['GPIO_INPUT']
     for pin in output_pins:
         pi.write(pin, 0)
-    for pin in input_pins:
-        pi.write(pin, 0)
-    print("All pins to ground")
+    print("All pins to 0")
