9380f805dc56c83e14fc4ee5fe4a3a180e64db51
Samuel Parra <samuelparra1@gmail.com>
2018-04-16 19:24:01 -0400

    ehhh

--- robotme/__init__.py
+++ robotme/__init__.py
@@ -5,7 +5,7 @@ app.config.from_object('config')
  """
 app = Flask(__name__)
 app.config.from_object('config')
-app.debug = False
+app.debug = True
 async_mode = None
 socketio = SocketIO(app, async_mode='eventlet')
 
